9a46de97d69c4f355099f23eef2755aaf1d202bd
industrious-chip-8083 <ufumakanneka@gmail.com>
2020-10-28 20:49:38 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -14,9 +14,9 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board) %2 == 0 
+  if turn_count(board) % 2 == 0 
     puts "X"
-  elsif turn_count(board) %2 == 1 
+  elsif turn_count(board) % 2 == 1 
     puts "O"
   end
 end
